066b065334bfb239842164b18d92201c8cc6f9ac
Julije Kostov <julijekos@gmail.com>
2022-11-17 22:46:56 +0100

    Initial commit

---
+++ src/secrets.ts
@@ -0,0 +1,20 @@
+import { DaprClient, CommunicationProtocolEnum } from '@dapr/dapr';
+
+const secretStoreName = process.env.SECRET_STORE_NAME ?? 'secretstore';
+
+export const getSecret = async (key: string): Promise<string | null> => {
+    const client = new DaprClient(undefined, undefined, CommunicationProtocolEnum.GRPC);
+
+    try {
+        const secretData: any = await client.secret.get(secretStoreName, key);
+
+        if (secretData?.[key]) {
+            return secretData[key] as string;
+        }
+    } catch {
+        return null;
+    }
+
+
+    return null;
+}
